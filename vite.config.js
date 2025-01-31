f88555318e02915ec1f75c189099a7ce475d7f3c
Indroneel28 <122139794+Indroneel28@users.noreply.github.com>
2025-01-31 21:38:54 +0530

    Adding files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/NeelGPT/', 
+  base: 'NeelGPT', 
   plugins: [react()],
   build: {
     outDir: 'dist'
